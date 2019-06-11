a2717456f1822be7f80852b0854bc729a2ff4711
Christoph Boerlin <borlinc@chalmers.se>
2019-06-11 14:41:03 +0200

    Added SNR filtering for Peak detection

--- PythonScripts/mapGEMpeaks.py
+++ PythonScripts/mapGEMpeaks.py
@@ -22,29 +22,33 @@ TSSData=[[x for x in line.rstrip('\n\r').split('\t')] for line in open(pathToTSS
 TSSData_genes=[x[0] for x in TSSData]
 
 if os.path.isfile(pathToGEM):
-	#load GEM data
-	gemData=pd.read_csv(pathToGEM,sep='\t')
-	#get conditions
-	condListGEM=[x for x in gemData.columns.tolist() if bool(re.match('Cond[a-zA-Z0-9]*_Present',x))]
-	condList=[x.replace('Cond','').replace('_Present','') for x in condListGEM]
-	#gemData=gemData.loc[:,['Position']+condListGEM]
-	#create outputTable
-	outputData=pd.DataFrame(0,index=TSSData_genes,columns=condList)
-	outputDataDetailed=pd.DataFrame(0,index=[],columns=['Gene','Chr','Pos','Strand','DistanceTSS','Strength','Condition'])
-	#iterate through GEM peaks, assign to genes
-	for i in range(0,len(gemData)):
-		selChr='chr'+str(gemData.iloc[i]['Position'].split(':')[0])
-		selPos=int(gemData.iloc[i]['Position'].split(':')[1])
-		#find gene
-		for gene in TSSData:
-			if selChr==gene[1] and abs(selPos-int(gene[2]))<=1000:
-				for cond in condList:
-					if gemData.iloc[i]['Cond'+cond+'_Present']==1:
-						outputDataDetailed.loc[len(outputDataDetailed)+1]=[gene[0],selChr,selPos,gene[3],abs(selPos-int(gene[2])),gemData.iloc[i]['Cond'+cond+'_IP'],cond]
-						outputData.loc[gene[0],cond]+=gemData.iloc[i]['Cond'+cond+'_Present']
+    #load GEM data
+    gemData=pd.read_csv(pathToGEM,sep='\t')
+    #get conditions
+    condListGEM=[x for x in gemData.columns.tolist() if bool(re.match('Cond[a-zA-Z0-9]*_Present',x))]
+    condList=[x.replace('Cond','').replace('_Present','') for x in condListGEM]
+    #gemData=gemData.loc[:,['Position']+condListGEM]
+    #create outputTable
+    outputData=pd.DataFrame(0,index=TSSData_genes,columns=condList)
+    outputDataSNR=pd.DataFrame(0,index=TSSData_genes,columns=condList) 
+    outputDataDetailed=pd.DataFrame(0,index=[],columns=['Gene','Chr','Pos','Strand','DistanceTSS','Strength','Condition'])
+    #iterate through GEM peaks, assign to genes
+    for i in range(0,len(gemData)):
+        selChr='chr'+str(gemData.iloc[i]['Position'].split(':')[0])
+        selPos=int(gemData.iloc[i]['Position'].split(':')[1])
+        #find gene
+        for gene in TSSData:
+            if selChr==gene[1] and abs(selPos-int(gene[2]))<=1000:
+                for cond in condList:
+                    if gemData.iloc[i][cond+'_IP']/gemData.iloc[i][cond+'_Expectd']>2:
+                        outputDataDetailed.loc[len(outputDataDetailed)+1]=[gene[0],selChr,selPos,gene[3],abs(selPos-int(gene[2])),gemData.iloc[i]['Cond'+cond+'_IP']/gemData.iloc[i][cond+'_Expectd'],cond]
+                        outputData.loc[gene[0],cond]+=gemData.iloc[i]['Cond'+cond+'_Present']
+                        outputDataSNR.loc[gene[0],cond]+=gemData.iloc[i]['Cond'+cond+'_IP']/gemData.iloc[i][cond+'_Expectd']
 
-	outputData=outputData.loc[outputData.sum(axis=1)>0]
-	outputData.to_csv(pathForOutput+'/'+tf+'_geneTargetList_'+date+'.csv')
-	outputDataDetailed.to_csv(pathForOutput+'/'+tf+'_GEManalysis_'+date+'.csv')
+    outputData=outputData.loc[outputData.sum(axis=1)>0]
+    outputDataSNR=outputDataSNR.loc[outputDataSNR.sum(axis=1)>0]
+    outputDataSNR.to_csv(pathForOutput+'/'+tf+'_geneTargetListSNR_'+date+'.csv')
+    outputData.to_csv(pathForOutput+'/'+tf+'_geneTargetList_'+date+'.csv')
+    outputDataDetailed.to_csv(pathForOutput+'/'+tf+'_GEManalysis_'+date+'.csv')
 else:
-	print('GEM file does not exist')
+    print('GEM file does not exist')
