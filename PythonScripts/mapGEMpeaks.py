Fix: Peak Distance to TSS Calculation

PythonScripts/mapGEMpeaks.py
@@ -41,9 +41,12 @@ if os.path.isfile(pathToGEM):
             if selChr==gene[1] and abs(selPos-int(gene[2]))<=1000:
                 for cond in condList:
                     if gemData.iloc[i]['Cond'+cond+'_IP']/gemData.iloc[i]['Cond'+cond+'_Expectd']>2:
-                        outputDataDetailed.loc[len(outputDataDetailed)+1]=[gene[0],selChr,selPos,gene[3],abs(selPos-int(gene[2])),gemData.iloc[i]['Cond'+cond+'_IP']/gemData.iloc[i]['Cond'+cond+'_Expectd'],cond]
                         outputData.loc[gene[0],cond]+=gemData.iloc[i]['Cond'+cond+'_Present']
                         outputDataSNR.loc[gene[0],cond]+=gemData.iloc[i]['Cond'+cond+'_IP']/gemData.iloc[i]['Cond'+cond+'_Expectd']
+                        if gene[3]=='+':
+                            outputDataDetailed.loc[len(outputDataDetailed)+1]=[gene[0],selChr,selPos,gene[3],selPos-int(gene[2]),gemData.iloc[i]['Cond'+cond+'_IP']/gemData.iloc[i]['Cond'+cond+'_Expectd'],cond]
+                        else:
+                            outputDataDetailed.loc[len(outputDataDetailed)+1]=[gene[0],selChr,selPos,gene[3],int(gene[2])-selPos,gemData.iloc[i]['Cond'+cond+'_IP']/gemData.iloc[i]['Cond'+cond+'_Expectd'],cond]
 
     outputData=outputData.loc[outputData.sum(axis=1)>0]
     outputDataSNR=outputDataSNR.loc[outputDataSNR.sum(axis=1)>0]